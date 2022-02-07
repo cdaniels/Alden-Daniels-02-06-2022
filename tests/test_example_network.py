8f4aab7b6f34e17aedf54e01a0f43d445337bb85
Alden <aldendaniels.home@gmail.com>
2022-02-06 23:45:16 -0500

    tests(nodes): add test for node creation

--- tests/test_example_network.py
+++ tests/test_example_network.py
@@ -1,6 +1,8 @@
 # test_example_network.py
 
 import unittest
+import random
+
 from src.example_network_package import example_network
 
 class TestExampleNetwork(unittest.TestCase):
@@ -10,6 +12,18 @@ class TestExampleNetwork(unittest.TestCase):
         # it should have an empty array of nodes and edges
         self.assertEqual(graph.nodes, [])
         self.assertEqual(graph.edges, [])
+
+
+    def test_node_creation(self):
+        # given the node creation funtion is called with a certain number
+        num = random.randint(1,100)
+        G = example_network.Graph()
+
+        G.add_node(num)
+        # when the nodes are checked
+        nodes = G.nodes
+        # then there should be a node with the corresponding number
+        self.assertEqual(nodes, [num])
 
 if __name__ == '__main__':
     unittest.main()
