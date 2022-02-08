a1d6cbe598122ed577581017070757a328ae85a8
Alden <aldendaniels.home@gmail.com>
2022-02-08 10:58:07 -0500

    tests(nodes): add failing test for batch node creation with attribute data

--- tests/test_example_network.py
+++ tests/test_example_network.py
@@ -1,17 +1,21 @@
 # test_example_network.py
 
-import unittest
-import random
+import unittest, random, string
 
 from src.example_network_package import example_network
+
+def random_word(length):
+   letters = string.ascii_lowercase
+   rand_word = ''.join(random.choice(letters) for i in range(length))
+   return rand_word
 
 class TestExampleNetwork(unittest.TestCase):
     def test_graph_instantiation(self):
         # given the graph class, when it is instantiated
         graph = example_network.Graph()
         # it should have an empty array of nodes and edges
-        self.assertEqual(graph.nodes, [])
-        self.assertEqual(graph.edges, [])
+        self.assertEqual(graph.nodes(), [])
+        self.assertEqual(graph.edges(), [])
 
 
     def test_node_creation(self):
@@ -21,7 +25,7 @@ class TestExampleNetwork(unittest.TestCase):
 
         G.add_node(num)
         # when the nodes are checked
-        nodes = G.nodes
+        nodes = G.nodes()
         # then there should be a node with the corresponding number
         self.assertEqual(nodes, [num])
 
@@ -31,16 +35,40 @@ class TestExampleNetwork(unittest.TestCase):
         rand_length = random.randint(1,100)
         node_list = []
         for i in range(rand_length):
-            rand_num = random.randint(1,100)
-            node_list.append(rand_num)
+            node_list.append(i)
 
         # when the batch node creation function is called with this array
         G = example_network.Graph()
         G.add_nodes_from(node_list)
 
         # then the graph should contain nodes with the corresponding numbers
-        nodes = G.nodes
+        nodes = G.nodes()
         self.assertCountEqual(nodes, node_list)
+
+
+    def test_batch_node_creation_with_attributes(self):
+        # given an array of number-attribute tuples of a certain length
+        rand_length = random.randint(1,100)
+        node_list = []
+        test_num = random.randint(1,100)
+        test_attr_name = random_word(5)
+        test_attr_val = random_word(5)
+
+        for i in range(rand_length):
+            rand_attr_name = random_word(5)
+            rand_attr_val = random_word(5)
+            node_attr_tuple = (i, {rand_attr_name: rand_attr_val})
+            node_list.append(node_attr_tuple)
+        node_attr_tuple = (test_num, {test_attr_name: test_attr_val})
+        node_list.append(node_attr_tuple)
+
+        # when the batch node creation function is called with this array
+        G = example_network.Graph()
+        G.add_nodes_from(node_list)
+
+        # then the graph should contain nodes with data corresponding to what was passed
+        node_data = G.node_data(test_num)
+        self.assertEqual(node_data[test_attr_name], test_attr_val)
 
 if __name__ == '__main__':
     unittest.main()
