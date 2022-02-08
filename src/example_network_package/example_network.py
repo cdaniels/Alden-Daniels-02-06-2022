22f2d6d0a8e60ea5dc59dc6b3c4dace035d41fb8
Alden <aldendaniels.home@gmail.com>
2022-02-08 10:58:30 -0500

    feat(nodes): implement attribute data for nodes

--- src/example_network_package/example_network.py
+++ src/example_network_package/example_network.py
@@ -2,12 +2,29 @@
 
 class Graph:
     def __init__(self):
-        self.nodes = list([])
-        self.edges = list([])
+        self.nodes_dict = {}
+        self.edges_dict = {}
 
     def add_node(self, node_value):
-        self.nodes.append(node_value)
+        self.nodes_dict[node_value] = {}
 
     def add_nodes_from(self, node_list):
         for node_value in node_list:
-            self.nodes.append(node_value)
+            if type(node_value) == tuple:
+                self.nodes_dict[node_value[0]] = node_value[1]
+            elif type(node_value) == int:
+                self.nodes_dict[node_value] = {}
+
+    def nodes(self):
+        node_list = list(self.nodes_dict.keys())
+        return node_list
+
+
+    def edges(self):
+        node_list = list(self.edges_dict.keys())
+        return node_list
+
+    def node_data(self, node_num):
+        return self.nodes_dict[node_num]
+
+
